632708b078a935b03649b46a7f8c9b9f489ee81a
rclc <cchiang@stthomas.edu>
2025-01-30 11:32:28 -0600

    Update lambda_function.py
    The function now can process either
    1. Notification directly from S3 bucket. (commented in this commit)
    2. SQS messages which contains an S3 notification wrapped in an SNS message. (active in this commit)

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
-import boto3, os, sys, uuid
+import boto3, os, sys, uuid, json
 from PIL import Image
 import PIL.Image
 
@@ -11,8 +11,20 @@ def resize_image(image_path, resized_path):
 
 def lambda_handler(event, context):
     for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
+        # If the event is directly from S3, use the following 2 lines
+        # bucket = record['s3']['bucket']['name']        
+        # key = record['s3']['object']['key']
+        
+        # If the event is fan-out from SQS, use line 19-26
+        # Parse the SQS message body
+        message_body = json.loads(record['body'])
+        # Parse the s3 event wrapped in SNS message body
+        s3_events = json.loads(message_body['Message'])['Records']
+        # Extract the S3 bucket name from the message body
+        for s3_event in s3_events:
+                bucket = s3_event['s3']['bucket']['name']
+                key = s3_event['s3']['object']['key']
+
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
         upload_path = '/tmp/resized-{}'.format(key)
 
